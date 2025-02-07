fd48875a645b88271462b3f401dcbb88177ccfab
Arhan Busam <arhan.busam@gmail.com>
2025-02-07 14:52:10 +1100

    Updated navigation

--- main.py
+++ main.py
@@ -8,46 +8,70 @@ import motor
 
 async def main():
     moving = False
-    direction = 0 # 0: forwards, 1: right, 2: back, 3: left
+    direction = 0 # degrees, from 0-359
     while True:
         if (button.pressed(button.RIGHT)):
             moving = not moving
             while (button.pressed(button.RIGHT)):
                 pass
         if (button.pressed(button.LEFT)):
-            direction += 1
-            direction = direction % 4
             if (direction == 0):
-                light_matrix.write("F")
-            elif (direction == 1):
+                direction = 90
                 light_matrix.write("R")
-            elif (direction == 2):
+            elif (direction == 90):
+                direction = 180
                 light_matrix.write("B")
-            elif (direction == 3):
+            elif (direction == 180):
+                direction = 270
                 light_matrix.write("L")
+            elif (direction == 270):
+                direction = 0
+                light_matrix.write("F")
+
             while (button.pressed(button.LEFT)):
                 pass
         if (moving == True):
-            if (direction == 0):
+            if (direction >= 0 and direction < 45):
                 motor.run(port.A, -1110)
-                motor.run(port.B, 1110)
                 motor.run(port.C, 1110)
-                motor.run(port.D, -1110)
-            elif (direction == 1):
+                motor.run(port.B, (1-(direction/45))*1110)
+                motor.run(port.D, (1-(direction/45))*-1110)
+            elif (direction >= 45 and direction < 90):
                 motor.run(port.A, -1110)
-                motor.run(port.B, -1110)
                 motor.run(port.C, 1110)
-                motor.run(port.D, 1110)
-            elif (direction == 2):
-                motor.run(port.A, 1110)
+                motor.run(port.B, ((direction-45)/45)*-1110)
+                motor.run(port.D, ((direction-45)/45)*1110)
+            elif (direction >= 90 and direction < 135):
                 motor.run(port.B, -1110)
-                motor.run(port.C, -1110)
                 motor.run(port.D, 1110)
-            elif (direction == 3):
-                motor.run(port.A, 1110)
-                motor.run(port.B, 1110)
-                motor.run(port.C, -1110)
-                motor.run(port.D, -1110)
+                motor.run(port.A, (1-((direction-90)/45))*-1110)
+                motor.run(port.C, (1-((direction-90)/45))*1110)
+            elif (direction >= 135 and direction < 180):
+                motor.run(port.B, -1110)
+                motor.run(port.D, 1110)
+                motor.run(port.A, ((direction-135)/45)*1110)
+                motor.run(port.C, ((direction-135)/45)*-1110)
+            elif (direction >= 180 and direction < 225):
+                motor.run(port.B, -1110)
+                motor.run(port.D, 1110)
+                motor.run(port.A, (1-(direction-180)/45)*1110)
+                motor.run(port.C, (1-(direction-180)/45)*-1110)
+            elif (direction >= 225 and direction < 270):
+                motor.run(port.B, -1110)
+                motor.run(port.D, 1110)
+                motor.run(port.A, ((direction-225)/45)*-1110)
+                motor.run(port.C, ((direction-225)/45)*1110)
+            elif (direction >= 270 and direction < 315):
+                motor.run(port.A, -1110)
+                motor.run(port.C, 1110)
+                motor.run(port.B, (1-((direction-270)/45))*1110)
+                motor.run(port.D, (1-((direction-270)/45))*-1110)
+            elif (direction >= 315 and direction < 360):
+                motor.run(port.A, -1110)
+                motor.run(port.C, 1110)
+                motor.run(port.B, ((direction-315)/45)*-1110)
+                motor.run(port.D, ((direction-315)/45)*1110)
+
         else:
             motor.stop(port.A)
             motor.stop(port.B)
