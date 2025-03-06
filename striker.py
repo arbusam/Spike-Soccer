2b5eb928cf362df4c7e4f5090d406fc9587bda20
Arhan Busam <arhan.busam@gmail.com>
2025-03-06 15:45:18 +1100

    Adjusted values and added ultrasonic check

--- striker.py
+++ striker.py
@@ -1,64 +1,64 @@
-from hub import motion_sensor, port, button
-import color_sensor
+from hub import motion_sensor, port
+import color_sensor, distance_sensor
 import runloop
 import motor
 
 def move(direction, speed, yaw):
     if (yaw > 200):
         motor.run(port.C, -1110)
-        motor.run(port.B, -1110)
         motor.run(port.D, -1110)
         motor.run(port.A, -1110)
+        motor.run(port.B, -1110)
     elif (yaw < -200):
         motor.run(port.C, 1110)
-        motor.run(port.B, 1110)
         motor.run(port.D, 1110)
         motor.run(port.A, 1110)
+        motor.run(port.B, 1110)
     elif (direction >= 0 and direction < 45):
-        motor.run(port.A, -speed)
-        motor.run(port.D, speed)
-        motor.run(port.B, int((1-(direction/45))*speed))
-        motor.run(port.C, int((1-(direction/45))*-speed))
+        motor.run(port.C, -speed)
+        motor.run(port.B, speed)
+        motor.run(port.D, int((1-(direction/45))*speed))
+        motor.run(port.A, int((1-(direction/45))*-speed))
     elif (direction >= 45 and direction < 90):
-        motor.run(port.A, -speed)
-        motor.run(port.D, speed)
-        motor.run(port.B, int(((direction-45)/45)*-speed))
-        motor.run(port.C, int(((direction-45)/45)*speed))
+        motor.run(port.C, -speed)
+        motor.run(port.B, speed)
+        motor.run(port.D, int(((direction-45)/45)*-speed))
+        motor.run(port.A, int(((direction-45)/45)*speed))
     elif (direction >= 90 and direction < 135):
-        motor.run(port.B, -speed)
-        motor.run(port.C, speed)
-        motor.run(port.A, int((1-((direction-90)/45))*-speed))
-        motor.run(port.D, int((1-((direction-90)/45))*speed))
+        motor.run(port.D, -speed)
+        motor.run(port.A, speed)
+        motor.run(port.C, int((1-((direction-90)/45))*-speed))
+        motor.run(port.B, int((1-((direction-90)/45))*speed))
     elif (direction >= 135 and direction < 180):
-        motor.run(port.B, -speed)
-        motor.run(port.C, speed)
-        motor.run(port.A, int(((direction-135)/45)*speed))
-        motor.run(port.D, int(((direction-135)/45)*-speed))
+        motor.run(port.D, -speed)
+        motor.run(port.A, speed)
+        motor.run(port.C, int(((direction-135)/45)*speed))
+        motor.run(port.B, int(((direction-135)/45)*-speed))
     elif (direction >= 180 and direction < 225):
-        motor.run(port.A, speed)
-        motor.run(port.D, -speed)
-        motor.run(port.B, int((1-(direction-180)/45)*-speed))
-        motor.run(port.C, int((1-(direction-180)/45)*speed))
+        motor.run(port.C, speed)
+        motor.run(port.B, -speed)
+        motor.run(port.D, int((1-(direction-180)/45)*-speed))
+        motor.run(port.A, int((1-(direction-180)/45)*speed))
     elif (direction >= 225 and direction < 270):
-        motor.run(port.A, speed)
-        motor.run(port.D, -speed)
-        motor.run(port.B, int(((direction-225)/45)*speed))
-        motor.run(port.C, int(((direction-225)/45)*-speed))
+        motor.run(port.C, speed)
+        motor.run(port.B, -speed)
+        motor.run(port.D, int(((direction-225)/45)*speed))
+        motor.run(port.A, int(((direction-225)/45)*-speed))
     elif (direction >= 270 and direction < 315):
-        motor.run(port.B, speed)
-        motor.run(port.C, -speed)
-        motor.run(port.A, int((1-((direction-270)/45))*speed))
-        motor.run(port.D, int((1-((direction-270)/45))*-speed))
+        motor.run(port.D, speed)
+        motor.run(port.A, -speed)
+        motor.run(port.C, int((1-((direction-270)/45))*speed))
+        motor.run(port.B, int((1-((direction-270)/45))*-speed))
     elif (direction >= 315 and direction < 360):
-        motor.run(port.B, speed)
-        motor.run(port.C, -speed)
-        motor.run(port.A, int(((direction-315)/45)*-speed))
-        motor.run(port.D, int(((direction-315)/45)*speed))
+        motor.run(port.D, speed)
+        motor.run(port.A, -speed)
+        motor.run(port.C, int(((direction-315)/45)*-speed))
+        motor.run(port.B, int(((direction-315)/45)*speed))
     else:
-        motor.stop(port.C)
-        motor.stop(port.B)
-        motor.stop(port.D)
         motor.stop(port.A)
+        motor.stop(port.D)
+        motor.stop(port.B)
+        motor.stop(port.C)
 
 async def main():
     DIRECTION_OFFSET = 10
@@ -69,26 +69,36 @@ async def main():
         irDirection = data[1]
         strength = data[0]
 
-        if strength > 70:
-            # Kicker
-            pass
+        if (irDirection != 0):
+            irDirection -= 1
+            if (irDirection == 0):
+                irDirection = 12
 
-        if irDirection == 8 or irDirection == 9 or irDirection == 3 or irDirection == 4 and strength >= 65:
+        if (irDirection == 8 or irDirection == 9 or irDirection == 3 or irDirection == 4) and strength >= 65:
             direction = 180
         elif irDirection == 5 and strength >= 60:
             direction = 225
-        elif irDirection == 7 and strength >= 60:
-            direction = 145
-        elif irDirection == 6 and strength >= 45:
-            direction = 120
+        elif irDirection == 7 and strength >= 60: # If ultrasonic reading is > 25
+            if (distance_sensor.distance(port.E) > 25):
+                direction = 145
+            else:
+                direction = 270
+        elif irDirection == 6 and strength >= 45: # If ultrasonic reading is > 90 go right, else go left
+            if (distance_sensor.distance(port.E) > 90):
+                direction = 120
+            else:
+                direction = 240
         else:
             direction = 360/12*irDirection
-        direction = 360-direction
-        direction += DIRECTION_OFFSET
-        if direction < 0:
-            direction = 360+direction
-        elif direction > 360:
-            direction -= 360
+        if direction == 360:
+            direction = 0
+            speed = 1110
+        elif direction == 0:
+            direction = 180
+            speed = 500
+        else:
+            speed = 1110
+
         if irDirection == 0:
             direction = 180
             speed = 500
